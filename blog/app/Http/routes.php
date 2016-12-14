d61837d10475bb63ce65d73b8eac620c99152b6c
VanushAtanesian <oganesovi4@mail.ru>
2016-12-14 20:56:49 +0200

    form

--- blog/app/Http/routes.php
+++ blog/app/Http/routes.php
@@ -1,16 +1,22 @@
 <?php
 
 /*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
+  |--------------------------------------------------------------------------
+  | Application Routes
+  |--------------------------------------------------------------------------
+  |
+  | Here is where you can register all of the routes for an application.
+  | It's a breeze. Simply tell Laravel the URIs it should respond to
+  | and give it the controller to call when that URI is requested.
+  |
+ */
 
 Route::get('/', function () {
     return view('welcome');
+});
+Route::group(['prefix' => 'admin/product'], function() {
+    Route::get('/', 'AdminController@index')->name('admin');
+    Route::get('/create', 'AdminController@create');
+    Route::post('/', 'AdminController@store');
+    Route::delete('/{product}','AdminController@destroy');
 });
---
+++ blog/resources/views/products/admin/create.blade.php
@@ -0,0 +1,33 @@
+@extends('layouts.admin')
+@section('content')
+@include('common.errors')
+<!-- Bootstrap шаблон... -->
+<div class="panel-body">
+    <form action="/admin/product" method="POST" class="form-horizontal">
+        {{ csrf_field() }}
+        <!-- Имя задачи -->
+        <div class="form-group">
+            <label for="task" class="col-sm-3 control-label">Name</label>
+
+            <div class="col-sm-6">
+                <input type="text" name="name" id="name" class="form-control">
+            </div>
+        </div>
+        <div class="form-group">
+            <label for="description" class="col-sm-3 control-label">Description</label>
+
+            <div class="col-sm-6">
+                <textarea name="description" id="task-name" class="form-control"></textarea>
+            </div>
+        </div>
+        <!-- Кнопка добавления задачи -->
+        <div class="form-group">
+            <div class="col-sm-offset-3 col-sm-6">
+                <button type="submit" class="btn btn-default">
+                    <i class="fa fa-plus"></i> Create product
+                </button>
+            </div>
+        </div>
+    </form>
+</div>
+@endsection
